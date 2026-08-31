670ef8c56fcd05d1e29c2dd4b27ec60d0dae5c2d
Egor <egorkarl10@gmail.com>
2026-08-31 11:57:28 +0300

    auth_system/urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('games/', views.games, name='games'),
     path('movies/', views.movies, name='movies'),
     path('originals/', views.originals, name='originals'),
+    path('', include('auth_system.urls')),
 ]
