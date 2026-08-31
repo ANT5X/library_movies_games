05df7d21b609d6c25e50d6009041e679274a3b08
Egor <egorkarl10@gmail.com>
2026-08-31 13:39:51 +0300

    fixes

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 
 
@@ -10,10 +11,26 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')  # Redirect to a success page after registration
+            return redirect('home') 
     else:
         form = UserCreationForm()
+
+
     return render(
         request,
         template_name='portal/auth.system/register.html', 
         context={'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request,username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Invalid username or password.")
